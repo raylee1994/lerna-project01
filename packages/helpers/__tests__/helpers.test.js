a86adc946f6beb2dbe65a97d67ee179be2790c10
raylee1994 <849103991@qq.com>
2022-10-26 23:17:55 +0800

    first commit

---
+++ packages/helpers/__tests__/helpers.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const helpers = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(helpers(), 'Hello from helpers');
+console.info("helpers tests passed");
---
+++ packages/helpers/lib/helpers.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = helpers;
+
+function helpers() {
+    return "Hello from helpers";
+}
